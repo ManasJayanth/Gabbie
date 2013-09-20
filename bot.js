8e5863b6a4746ab2e2989d2d972f0c113d11db03
Manas <harlequinsguitar@gmail.com>
2013-09-20 21:55:31 +0530

    Uncommented necessary code

--- bot.js
+++ bot.js
@@ -11,12 +11,12 @@ var irc = require("irc");
 var bot = new irc.Client(config.server,config.botName,{
 	channels: config.channels});
 // Listen for joins
-// bot.addListener("join", function(channel, who) {
-// 	// Welcome them in!
-// 	if(who != 'Gabbie')
-// 	bot.say(channel, "Welcome " + who + ". I'm Gabbie.");
-// });
-// Listen for any message, PM said user when he posts
+bot.addListener("join", function(channel, who) {
+	// Welcome them in!
+	if(who != 'Gabbie')
+	bot.say(channel, "Welcome " + who + ". I'm Gabbie.");
+});
+Listen for any message, PM said user when he posts
 bot.addListener("pm", function(from, to, text, message) {
 	bot.say(from, "Hi! I'm Gabbie. I'm Manas's friend. He'll tell you more about me");
 });
